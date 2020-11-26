e4b7a6f564a819e6500f4de629417edd406546f6
Kritanta <KritantaDevelopment@gmail.com>
2020-11-26 02:45:13 -0600

    Update MSHBarView.h

--- MSHBarView.h
+++ MSHBarView.h
@@ -1,8 +1,10 @@
 #include "MSHView.h"
-
+#ifndef MSHBARVIEWH
+#define MSHBARVIEWH
 @interface MSHBarView : MSHView
 
 @property (nonatomic, assign) CGFloat barCornerRadius;
 @property (nonatomic, assign) CGFloat barSpacing;
 
 @end
+#endif
